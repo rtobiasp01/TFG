3bcd78a6f7724e8ee47292db7cf0b875a5ae8bb4
rtobiasp01 <161591413+rtobiasp01@users.noreply.github.com>
2026-02-16 21:41:55 +0100

    Add price sorting to products list
    Wire Price column to a new ordenarPrecio() handler and implement client-side sorting. home.html: add (click) on Price header. home.ts: import rxjs map (and filter), introduce filteredProducts$ initialized from products$, and add ordenarPrecio() which maps the product stream to an immutably sorted array by price (ascending). This enables clicking the header to sort the displayed products by price.

--- WEB/src/app/pages/home/home.ts
+++ WEB/src/app/pages/home/home.ts
@@ -2,7 +2,7 @@ import { Component, inject } from '@angular/core';
 import { ProductService } from '../../services/product-service';
 import { AsyncPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { Observable } from 'rxjs';
+import { filter, map, Observable } from 'rxjs';
 import { Product } from '../../interfaces/product';
 import { CurrencyPipe } from '@angular/common';
 
@@ -17,4 +17,12 @@ export class Home {
   private productService = inject(ProductService);
 
   products$: Observable<Product[]> = this.productService.getAll();
+
+  filteredProducts$ = this.products$;
+
+  ordenarPrecio() {
+    this.filteredProducts$ = this.filteredProducts$.pipe(
+      map((products) => [...products].sort((a, b) => a.price - b.price)),
+    );
+  }
 }
