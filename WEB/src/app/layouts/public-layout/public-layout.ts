8098c75e6b7a8a1c20239c6351baaf7eded82c3b
Ruben <rtobiasp01@larioja.edu.es>
2026-04-10 09:12:52 +0200

    navbar

--- WEB/src/app/layouts/public-layout/public-layout.ts
+++ WEB/src/app/layouts/public-layout/public-layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from '../../public/components/navbar/navbar';
 
 @Component({
   selector: 'app-public-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './public-layout.html',
   styleUrl: './public-layout.css',
 })
